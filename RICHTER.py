300bf0673b84dbd4f0c1756ce79de5ef7b41f261
21368236 <43570788+21368236@users.noreply.github.com>
2018-10-18 15:35:04 +0100

    Add files via upload

---
+++ RICHTER.py
@@ -0,0 +1,13 @@
+richter = input("Richter scale")
+richter = int (richter)
+if (richter >= 8):
+    print ("Most structures fall")
+elif (richter >= 7):
+    print ("Many buildings destroyed")
+elif (richter >= 6):
+    print ("Many buildings considerably damaged, some collapse")
+elif (richter >= 4.5):
+    print ("Damage to poorly constructed buildings")
+else:
+    print ("Destruction of buildings most unlikely")
+richter = input("Richter scale")
---
+++ sssssssssssssssssssssssssssssssssss.py
@@ -0,0 +1,38 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:/Users/21368236/Desktop/DB7.py =================
+Traceback (most recent call last):
+  File "C:/Users/21368236/Desktop/DB7.py", line 3, in <module>
+    print (s/7)
+TypeError: unsupported operand type(s) for /: 'str' and 'int'
+>>> 
+=============== RESTART: C:/Users/21368236/Desktop/RICHTER.py ===============
+Traceback (most recent call last):
+  File "C:/Users/21368236/Desktop/RICHTER.py", line 3, in <module>
+    richter = int (richter)
+ValueError: invalid literal for int() with base 10: 'Richter scale:'
+>>> 
+=============== RESTART: C:/Users/21368236/Desktop/RICHTER.py ===============
+Traceback (most recent call last):
+  File "C:/Users/21368236/Desktop/RICHTER.py", line 3, in <module>
+    richter = int (richter)
+ValueError: invalid literal for int() with base 10: 'Richter scale'
+>>> 
+=============== RESTART: C:/Users/21368236/Desktop/RICHTER.py ===============
+Traceback (most recent call last):
+  File "C:/Users/21368236/Desktop/RICHTER.py", line 2, in <module>
+    richter = int (richter)
+ValueError: invalid literal for int() with base 10: 'Richter scale'
+>>> 
+=============== RESTART: C:/Users/21368236/Desktop/RICHTER.py ===============
+Richter scale5
+Damage to poorly constructed buildings
+>>> 7
+7
+>>> 
+=============== RESTART: C:/Users/21368236/Desktop/RICHTER.py ===============
+Richter scale7
+Many buildings destroyed
+Richter scale5
+>>> 
