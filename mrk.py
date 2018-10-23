f125453e4cad92f16b88e9d6c3b414d9c781c46d
21368236 <43570788+21368236@users.noreply.github.com>
2018-10-23 11:56:20 +0100

    Add files via upload

---
+++ mrk.py
@@ -0,0 +1,5 @@
+mark = int(input("ENTER O MARK!!!"))
+if mark >= 40:
+    print("It's a pass...")
+else:
+    print("This is below a pass...")
---
+++ zZz.py
@@ -0,0 +1,4 @@
+username = input("Username entry")
+residence = input ("Residence:")
+age= int(input("Age of user:"))
+print (username + " lives in " + residence + ". They will turn " + (age+1) " on their next birthday")
