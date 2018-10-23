0c77e978f4a9cf3eca6f4b6284301a15dd0ebb08
21368236 <43570788+21368236@users.noreply.github.com>
2018-10-23 10:50:15 +0100

    Add files via upload
    W6 is faulty for some reason.

---
+++ W6.py
@@ -0,0 +1,17 @@
+import random
+
+#Generate a random number...
+number =random.randint(1,100)
+print("guess a number betwixt 0 &100")
+
+guess = -1
+while guess != number:
+    guess = int(input("Enter the guess...")
+
+    if guess == number:
+    #Prompt the user...
+        print ("correct. The number is" + number)
+    elif  guess> number:
+        print("the correct answer was lower than that.")
+    else:
+        print ("The correct number was above that.")
