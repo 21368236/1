88f94b101d73925ad4dc4e4e7b9c90790531e877
21368236 <43570788+21368236@users.noreply.github.com>
2018-10-30 11:59:47 +0000

    Add files via upload

---
+++ w7.py
@@ -0,0 +1,19 @@
+a= [66.25,333,333,1,1234.5]
+print (a)
+a.insert(2,-1)
+print (a)
+a.append(333)
+print (a)
+a.remove(333)
+print (a)
+a.reverse()
+print (a)
+a.sort ()
+print (a)
+
+nums = []
+for x in [1,2,3]:
+    for y in [3,1,4]:
+        if x != y:
+            nums.append((x.y))
+print(nums)
