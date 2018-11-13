59906f17aad224068e377b6b1ab943313c8c7792
21368236 <43570788+21368236@users.noreply.github.com>
2018-11-13 10:47:33 +0000

    Update O1.py

--- O1.py
+++ O1.py
@@ -1,6 +1,6 @@
 username = input("Username entry")
 residence = input ("Residence:")
 age= int(input("Age of user:"))
-gender- (input("Gender (M/F):"))
+#gender- (input("Gender (M/F):"))
 
      print (username, " lives in ", residence, ". They will turn ", (age+1), " on their next birthday")
