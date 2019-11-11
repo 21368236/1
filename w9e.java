b966da3c95ab297e1ab6396c91e7bf4713b931ba
21368236 <43570788+21368236@users.noreply.github.com>
2019-11-11 13:09:14 +0000

    Create w9e.java

---
+++ w9e.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 21368236
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // initializing the array for candidates
+        int[]numList=[10];
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enta " + numList.length + " values: ");
+        for (int i = 0; i <numList.length; i++) {
+            numList[i] = nextInt();
+        }
+        int average = numList[0]
+        }
+        
+        
+        
+    }
+    
+}
