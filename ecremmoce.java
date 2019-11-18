034165964fff622dfb63c3dca455cd45889d9793
21368236 <43570788+21368236@users.noreply.github.com>
2019-11-18 13:01:36 +0000

    Update ecremmoce.java

--- ecremmoce.java
+++ ecremmoce.java
@@ -19,3 +19,42 @@ public class ECREMMOCE {
     }
     
 }
+------------------------------------------------------------------------------------------
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 21368236
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //
+        double 101= 0;
+        double 102= 0;
+        String user_in;
+        
+        System.out.println("tu:Or- 5 items currently provided: \n 1. a (599.99) \n 1. b (299.99) \n 1. c (999.99) \n 1. d (49.99) \n 1. e (149.99)");
+        
+        do{
+            //selection process
+            System.out.println("Product selection begins here");
+            String item = insert.next();
+            String
+            
+        }
+        
+                
+    }
+    
+}
