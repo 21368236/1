60359d5d6c77d7b5fb885a4d86e15d48359ec98a
21368236 <43570788+21368236@users.noreply.github.com>
2019-11-25 12:59:11 +0000

    Update ecremmoce.java

--- ecremmoce.java
+++ ecremmoce.java
@@ -21,10 +21,8 @@ public class ECREMMOCE {
 }
 ------------------------------------------------------------------------------------------
     /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- * practical aCtivity 4
+
+ * practical aCtivity 4 by 21368236
  */
 package javaapplication1;
 import java.util.Scanner;
@@ -41,11 +39,11 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         // TODO code application logic here
         //
-        double 101= 0;
-        double 102= 0;
+        double Cost= 0;
+        double kyGohoh= 0;
         String user_in;
         
-        System.out.println("tu:Or- 5 items currently provided: \n 1. a (599.99) \n 1. b (299.99) \n 1. c (999.99) \n 1. d (49.99) \n 1. e (149.99)");
+        System.out.println("tu:Or- 5 items currently provided: \n 1. arbelt (599.99) \n 1. bloph (299.99) \n 1. cronag (999.99) \n 1. disvenk (49.99) \n 1. eplectis (149.99)");
         
         do{
             //selection process
@@ -53,6 +51,30 @@ public class JavaApplication1 {
             String item = insert.next();
             System.out.println("The price of the selected product is...");
             double itemPrice = insert.nextDouble();
+            System.out.println("What is the quantity of the" + item + "(s)?");
+            int Quantity = insert.nextInt;
+            Cost = itemPrice*Quantity;
+            kyGohoh = Cost;
+            System.out.println("Want to continue shopping? (Y/N)");
+            if Quantity == 1{
+            System.out.print("\n A" + item + "has been purchased");
+                else if Quantity == 0{ 
+                    System.out.print("\n Nothing has been purchased");
+                    else {
+                        System.out.print("\n" + Quantity + item + "s have been purchased");
+                    }
+                }
+            }
+
+            System.out.print("\n costing" + Cost + "in total");
+        }
+            while(user_in.EqualsIgnoreCase) 
+                    
+                
+                
+            
+               
+                
             
             
         }
