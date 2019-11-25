59e53bb41a375bbf3c371881215e7c06eef6388b
21368236 <43570788+21368236@users.noreply.github.com>
2019-11-25 11:53:29 +0000

    Update ecremmoce.java

--- ecremmoce.java
+++ ecremmoce.java
@@ -24,6 +24,7 @@ public class ECREMMOCE {
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * practical aCtivity 4
  */
 package javaapplication1;
 import java.util.Scanner;
@@ -50,7 +51,9 @@ public class JavaApplication1 {
             //selection process
             System.out.println("Product selection begins here");
             String item = insert.next();
-            String
+            System.out.println("The price of the selected product is...");
+            double itemPrice = insert.nextDouble();
+            
             
         }
         
