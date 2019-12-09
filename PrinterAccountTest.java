be6be19b332458e2e911826c4caff4740b41136d
21368236 <43570788+21368236@users.noreply.github.com>
2019-12-09 11:50:56 +0000

    Create PrinterAccountTest.java

---
+++ PrinterAccountTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PrinterAccountTest;
+
+/**
+ *
+ * @author 21368236
+ */
+public class PrinterAccountTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+public class PrinterAccount{
+        public static void main(String[] args) {
+        // TODO code application logic here
+        String studentID;
+        String name;
+        int balance;
+        int pagesBalanced;
+        default constructor
+        public int getID()
+        }
+        
+        public int getBalance(){
+        return (balance);
+        }
+        
+        
+        
+        
+        
+        }
+        
+}
+}
