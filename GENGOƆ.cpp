f2515440109c9d3057ea86112d6aa82856a80862
21368236 <43570788+21368236@users.noreply.github.com>
2020-02-13 15:44:05 +0000

    Create GENGOƆ.cpp

---
+++ GENGOƆ.cpp
@@ -0,0 +1,53 @@
+// ConsoleApplication40.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+	enum gengor { 0, GX , MJ , TS , SW , HS , RW , QX};
+	int 
+
+	gengor cGengor = 0;
+
+	if (cWeap == unarmed) {
+		return
+
+
+	struct weapon {
+		string name;
+		int damage;
+		weaponType wt;
+	}
+	struct monster {
+		string name;
+		int hp, damage, armour;
+	}
+	wo1w;
+	o1w.name = "it";
+	o1w.damage = 23;
+	o1w.wt = axe;
+
+	monster orc;
+	orc.name = "Orc";
+	orc.hp = 20;
+	orc.w = 01w;*/
+	/*orc.armour = 5;*/
+
+	cout << "the weopon of Orc1 is called " << o1w.name << endl;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
